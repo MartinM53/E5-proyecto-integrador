313e48de639386bec4fdbf7b0730bc448a80cff1
Martin M <martin99953@hotmail.com>
2023-09-10 01:01:18 -0300

    4th

--- signup.js
+++ signup.js
@@ -28,3 +28,30 @@ const isEmailValid = (input) => {
   const re = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
   return re.test(input.value.trim());
 };
+
+// función para chequear si el mail ya existe en el array de usuarios
+const isExistingEmail = () => {
+  return users.some((user) => user.email === input.value.trim());
+};
+
+// función para validar una contraseña con expresión regular
+const isPassSecure = (input) => {
+  const re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$^&*()_-]).{8,18}$/;
+  return re.test(input.value.trim());
+};
+
+// función para validar el teléfono con expresión regular
+const isPhoneValid = (input) => {
+  const re = /^[0-9]{10}$/;
+  return re.test(input.value.trim());
+};
+
+// función para mostrar error al validar un input
+const shoError = (input, message) => {
+  const formField = input.parentElement;
+  formField.classList.remove(`success`);
+  formField.classList.add(`error`);
+  const error = formField.querySelector(`small`);
+  error.style.display = "block";
+  error.textContent = "message";
+};
